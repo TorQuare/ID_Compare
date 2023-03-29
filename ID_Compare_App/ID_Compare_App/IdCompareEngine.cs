693c26f8e63c86384dc603ddd6593556f784c3fd
Patryk Bajgot <45692984+TorQuare@users.noreply.github.com>
2023-03-30 00:27:39 +0200

    First push / Version C0.0.1 / Compare engine created
    Compare engine created.

---
+++ ID_Compare_App/ID_Compare_App/IdCompareEngine.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID_Compare_App
+{
+    public class IdCompareEngine
+    {
+        string[] array_to_compare;
+        string[] base_array;
+
+        public IdCompareEngine(string[] array_to_compare, string[] base_array) 
+        {
+            this.array_to_compare = array_to_compare;
+            this.base_array = base_array;
+            this.start_compare();
+        }
+
+        public string[] compare_duplicates(bool skip)
+        {
+            string[] found_duplicates = {};
+            foreach (string value in array_to_compare)
+            {
+                foreach (string base_value in base_array)
+                {
+                    if (value == base_value)
+                    {
+                        switch (skip)
+                        {
+                            case true:
+                                if (this.double_check(value, found_duplicates) == false)
+                                {
+                                    found_duplicates = found_duplicates.Append(value).ToArray();
+                                }
+                                break;
+                            
+                            case false:
+                                found_duplicates = found_duplicates.Append(value).ToArray();
+                                break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", found_duplicates));
+            return found_duplicates;
+        }
+
+        public string[] not_found()
+        {
+            bool found = false;
+            string[] not_found = {};
+            foreach (string value in array_to_compare)
+            {
+                foreach (string base_value in base_array)
+                {
+                    if (value == base_value)
+                    {
+                        found = true;
+                    }
+                }
+                if(found) { found = false; }
+                else { not_found = not_found.Append(value).ToArray(); }
+            }
+            Console.WriteLine(string.Join(" ", not_found));
+            return not_found;
+        }
+
+        private bool double_check(string value, string[] array_of_found_duplicates)
+        {
+            foreach (string check_value in array_of_found_duplicates)
+            {
+                if (value == check_value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void start_compare()
+        {
+            bool skip_duplicates = false;
+            for(int iterator = 0; iterator <= 1; iterator++)
+            {
+                if (skip_duplicates)
+                {
+                    string[] not_found = this.not_found();
+                }
+                string[] found_duplicates = this.compare_duplicates(skip_duplicates);
+                skip_duplicates = !skip_duplicates;
+            }
+            
+        }
+
+        
+    }
+}
---
+++ ID_Compare_App/ID_Compare_App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID_Compare_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array_to_compare = { "VW", "BMW", "123" };
+            string[] base_array = { "VW", "Audi", "123", "123" };
+            IdCompareEngine base_compare = new IdCompareEngine(array_to_compare, base_array);
+        }
+    }
+}
